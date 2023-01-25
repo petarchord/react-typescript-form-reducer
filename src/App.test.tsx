7fa1b87699a82154889e923fd1ba62852d6ce222
Petar Jovanovic <petar.jovanovic@symphony.is>
2023-01-25 10:14:37 +0100

    feat(tooling): added tests for App.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders the child component', () => {
+  const { getByText } = render(<App />)
+  expect(getByText(/Submit a Session!/)).toBeInTheDocument()
+})
