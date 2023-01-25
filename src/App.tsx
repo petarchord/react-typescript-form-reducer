267bd942bffbdcc4839c391cc64b4baa11a4978e
Petar Jovanovic <petar.jovanovic@symphony.is>
2023-01-25 09:54:25 +0100

    feat(tooling): added eslint & prettier config, husky githooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import "./App.css";
-import SessionForm from "./components/SessionForm";
+import './App.css';
+import SessionForm from './components/SessionForm';
 
 function App() {
+  console.log('Hello World!');
   return (
     <div className="main-container">
       <SessionForm />
